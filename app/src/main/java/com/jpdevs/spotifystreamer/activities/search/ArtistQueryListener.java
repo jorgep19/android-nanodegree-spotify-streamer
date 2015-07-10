52e9e344eec5d65529f98f2b3dd9e84209fd7e9d
jorgep19 <jorgelpaez19@gmail.com>
2015-07-09 22:49:56 -0500

    implemented timer flag for the query listener

--- app/src/main/java/com/jpdevs/spotifystreamer/activities/search/ArtistQueryListener.java
+++ app/src/main/java/com/jpdevs/spotifystreamer/activities/search/ArtistQueryListener.java
@@ -3,27 +3,53 @@ package com.jpdevs.spotifystreamer.activities.search;
 
 import android.support.v7.widget.SearchView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ArtistQueryListener implements SearchView.OnQueryTextListener {
     public interface SearchQueryListener {
         void performSearch(String query);
     }
 
     private SearchQueryListener mListener;
+    private boolean mShouldSearch;
+    private String mCurrentString;
+    private String mStringFromLastQuery;
 
     public ArtistQueryListener (SearchQueryListener listener) {
+        mShouldSearch = true;
         mListener = listener;
+        mCurrentString = "";
+
     }
 
     @Override
     public boolean onQueryTextSubmit(String s) {
         mListener.performSearch(s);
-        return false;
+        return true;
     }
 
     @Override
     public boolean onQueryTextChange(String s) {
-        // TODO check if appropriate based on time
-        mListener.performSearch(s);
-        return false;
+        mCurrentString  = s;
+
+        if (mShouldSearch) {
+            mShouldSearch = false;
+            mListener.performSearch(s);
+            mStringFromLastQuery = s;
+
+            Timer T = new Timer();
+            T.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mShouldSearch = true;
+                    if(!mCurrentString.equals(mStringFromLastQuery)) {
+                        mListener.performSearch(mCurrentString);
+                    }
+                }
+            }, 1000);
+        }
+
+        return true;
     }
 }
